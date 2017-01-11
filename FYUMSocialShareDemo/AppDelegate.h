944b1420d29d04bdfae9b17241df52995cb7e5a5
fanyang <1050549505@qq.com>
2017-01-11 09:13:07 +0800

    Initial Commit

---
+++ FYUMSocialShareDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FYUMSocialShareDemo
+//
+//  Created by 樊杨 on 2017/1/11.
+//  Copyright © 2017年 FY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FYUMSocialShareDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FYUMSocialShareDemo
+//
+//  Created by 樊杨 on 2017/1/11.
+//  Copyright © 2017年 FY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
